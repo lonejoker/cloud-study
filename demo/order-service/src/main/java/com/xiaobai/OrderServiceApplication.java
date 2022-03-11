e316a5a28776eda659809359dd86a8370dcbde50
xiaobai <1582294124@qq.com>
2022-03-11 23:50:49 +0800

    实现远程调用

--- demo/order-service/src/main/java/com/xiaobai/OrderServiceApplication.java
+++ demo/order-service/src/main/java/com/xiaobai/OrderServiceApplication.java
@@ -3,6 +3,8 @@ package com.xiaobai;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @MapperScan("com.xiaobai.mapper")
@@ -12,4 +14,11 @@ public class OrderServiceApplication {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
+    /**
+     * 创建RestTemplate并注入spring容器中
+    */
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
--- demo/order-service/src/main/java/com/xiaobai/service/OrderService.java
+++ demo/order-service/src/main/java/com/xiaobai/service/OrderService.java
@@ -2,8 +2,10 @@ package com.xiaobai.service;
 
 import com.xiaobai.mapper.OrderMapper;
 import com.xiaobai.pojo.Order;
+import com.xiaobai.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author 终于白发始于青丝
@@ -18,10 +20,19 @@ public class OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private RestTemplate restTemplate;
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+        // 2.利用RestTemplate发起http请求，查询用户
+        // 2.1url路径
+        String url = "http://localhost:8081/user/"+order.getUserId();
+        // 2.2发起http请求，实现远程调用
+        User user = restTemplate.getForObject(url, User.class);
+        // 3.封装user到order
+        order.setUser(user);
         // 4.返回
         return order;
     }
