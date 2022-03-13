119a5db1e62ef13e6e3c736afabf87292ef93f2e
xiaobai <1582294124@qq.com>
2022-03-13 19:17:09 +0800

    gateway入门及断言工厂

---
+++ demo/gateway/src/main/java/com/xiaobai/GateWayApplication.java
@@ -0,0 +1,19 @@
+package com.xiaobai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 终于白发始于青丝
+ * @create 2022-03-13 下午 19:01
+ * @program demo
+ * @Version 1.0
+ * @ClassName GateWayApplication
+ * @Description 类方法说明：$
+ */
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
